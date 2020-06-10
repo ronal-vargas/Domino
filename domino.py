3ac80945d9dbb4b2dbe84accc969594c7d37cc94
ronal-vargas <64703811+ronal-vargas@users.noreply.github.com>
2020-06-10 00:33:36 -0400

    Update domino.py

--- domino.py
+++ domino.py
@@ -1,30 +1,56 @@
-from itertools import combinations
-
-def perm1(lst):
-#    if len(lst) == 0:
-#        return[]
-#    elif len(lst) == 1:
-#        return [lst]
-#    else:
-        l = []
-        for i in range(len(lst)):
-            x = lst[i]
-            xs= lst[:i] + lst[i+1:]
-            for p in perm1(xs):
-                l.append([x]+p)
-        return l
-
-#data = list('abc')
-data = [1,2,3]
-#print perm1
-for p in perm1(data):
-    print (p)
+from itertools import permutations 
 
 
+Fichas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
 
-numeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
-#numeros = [1,2,3]
+M1 = []
+M2 = []
+M3 = []
+M4 = []
+MF = []
 
-for c in combinations(numeros,28):
 
-    print (c)
+#for f in range (28):
+
+    #print (f)
+
+perm = permutations(Fichas)
+
+for i in list(perm): 
+
+    #print (i) 
+
+    #if i == 30 return
+    
+    for f in range(0,7):
+
+        M1.append (f)
+        #print (f)
+
+    for f in range(7,14):
+
+        M2.append (f)
+        #print (f)
+
+    for f in range(14,21):
+
+        M3.append (f)
+        #print (f)
+
+    for f in range(21,28):
+        
+        M4.append (f)
+        #print (f)
+
+
+#print (M1)
+#print (M2)
+#print (M3)
+#print (M4)
+
+MF = M1 + M2 + M3 + M4
+
+print (MF)
+
+
+#return
